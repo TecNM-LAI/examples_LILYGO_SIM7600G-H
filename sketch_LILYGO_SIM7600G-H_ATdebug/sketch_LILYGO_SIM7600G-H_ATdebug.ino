5a5608e281272578c860674982a8d3329dfc36b6
LGVil97 <gerardo_vilchis97@hotmail.com>
2023-02-09 23:57:57 -0600

    Primera carga de archivos

---
+++ sketch_LILYGO_SIM7600G-H_ATdebug/sketch_LILYGO_SIM7600G-H_ATdebug.ino
@@ -0,0 +1,142 @@
+/*
+  FILE: ATdebug.ino
+  AUTHOR: Kaibin
+  PURPOSE: Test functionality
+*/
+
+#define uS_TO_S_FACTOR      1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP       30          /* Time ESP32 will go to sleep (in seconds) */
+
+#define UART_BAUD           115200
+
+#define MODEM_TX            27
+#define MODEM_RX            26
+#define MODEM_PWRKEY        4
+#define MODEM_DTR           32
+#define MODEM_RI            33
+#define MODEM_FLIGHT        25
+#define MODEM_STATUS        34
+
+#define SD_MISO             2
+#define SD_MOSI             15
+#define SD_SCLK             14
+#define SD_CS               13
+
+#define LED_PIN             12
+
+#define TINY_GSM_MODEM_SIM7600
+#define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
+#define SerialAT Serial1
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
+
+
+// set GSM PIN, if any
+#define GSM_PIN ""
+
+// Your GPRS credentials, if any
+const char apn[]  = "YOUR-APN";     //SET TO YOUR APN
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+#include <TinyGsmClient.h>
+#include <SPI.h>
+//#include <SD.h>
+#include <Ticker.h>
+//#include "utilities.h"
+
+
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, Serial);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+
+
+int counter, lastIndex, numberOfPieces = 24;
+String pieces[24], input;
+
+
+bool reply = false;
+
+void modem_on() {
+    /*
+    The indicator light of the board can be controlled
+    */
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+
+    /*
+    MODEM_PWRKEY IO:4 The power-on signal of the modulator must be given to it,
+    otherwise the modulator will not reply when the command is sent
+    */
+    pinMode(MODEM_PWRKEY, OUTPUT);
+    digitalWrite(MODEM_PWRKEY, HIGH);
+    delay(300); //Need delay
+    digitalWrite(MODEM_PWRKEY, LOW);
+
+    /*
+    MODEM_FLIGHT IO:25 Modulator flight mode control,
+    need to enable modulator, this pin must be set to high
+    */
+    pinMode(MODEM_FLIGHT, OUTPUT);
+    digitalWrite(MODEM_FLIGHT, HIGH);
+
+    
+  int i = 10;
+  Serial.println("\nTesting Modem Response...\n");
+  Serial.println("****");
+  while (i) {
+    SerialAT.println("AT");
+    delay(500);
+    if (SerialAT.available()) {
+      String r = SerialAT.readString();
+      Serial.println(r);
+      if ( r.indexOf("OK") >= 0 ) {
+        reply = true;
+        break;;
+      }
+    }
+    delay(500);
+    i--;
+  }
+  Serial.println("****\n");
+}
+
+void setup() {
+  Serial.begin(115200); // Set console baud rate
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(100);
+
+  modem_on();
+  if (reply) {
+    Serial.println(F("***********************************************************"));
+    Serial.println(F(" You can now send AT commands"));
+    Serial.println(F(" Enter \"AT\" (without quotes), and you should see \"OK\""));
+    Serial.println(F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
+    Serial.println(F(" DISCLAIMER: Entering AT commands without knowing what they do"));
+    Serial.println(F(" can have undesired consiquinces..."));
+    Serial.println(F("***********************************************************\n"));
+  } else {
+    Serial.println(F("***********************************************************"));
+    Serial.println(F(" Failed to connect to the modem! Check the baud and try again."));
+    Serial.println(F("***********************************************************\n"));
+  }
+}
+
+void loop() {
+  while (true) {
+    if (SerialAT.available()) {
+      Serial.write(SerialAT.read());
+    }
+    if (Serial.available()) {
+      SerialAT.write(Serial.read());
+    }
+    delay(1);
+  }
+
+}
